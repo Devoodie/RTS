8c41e2d24620864adddba37499f112f83fc0a524
Devoodie <devantecoley802@gmail.com>
2026-03-04 03:23:34 -0400

    TODO >> Check that grid has been initialized properly! Finished the initialize grid function!

--- src/grid_utils.c
+++ src/grid_utils.c
@@ -6,80 +6,40 @@ HexSpace default_hex = {
 	.neighbors = {NULL, NULL, NULL, NULL, NULL, NULL},
 };
 
-void initialize_grid(int row, int col, HexSpace (*grid)[col]){
+void initialize_grid(int row, int col, HexSpace grid[row][col]){
+	// i is our row (y) index
+	// j is our column (x) index
 	for(int i = 0; i < row; ++i){
 		for(int j = 0; j < col; j++){
 			HexSpace *CurrentHex = &grid[i][j];
 			*CurrentHex = default_hex;
 			
 			//cannot traverse off of the map 
-			if(j == 0){
-				CurrentHex->neighbors[3] = NULL;
-				CurrentHex->neighbors[4] = NULL;
-				CurrentHex->neighbors[5] = NULL;
-
-			} else {
-				void;
-			}
-
-			if(j == col - 1){
-
-			} else {
-
-			}
-
-			if(i == 0){
-
-			} else {
-
-			}
-
-			if(i == row - 1){
-
-			} else {
-
-			}
-
-		}
-	}
-
-	void;
-	for(int i = 0; i < row; ++i){
-		for(int j = 0; j < col; j++){
-			HexSpace *CurrentHex = &grid[i][j];
-			*CurrentHex = default_hex;
+			//
+			//remember, our hexagons are represented in a matrix
+			//the origin is the top left or first index
+			//therefore, north (or up) is -1 and south (down) is + 1 
+		
 			
-			//cannot traverse off of the map 
-			if(j == 0){
-				CurrentHex->neighbors[3] = NULL;
-				CurrentHex->neighbors[4] = NULL;
-				CurrentHex->neighbors[5] = NULL;
-
-			} else {
-				void;
-			}
-
-			if(j == col - 1){
-
-			} else {
-
-			}
-
-			if(i == 0){
-
-			} else {
-
-			}
-
-			if(i == row - 1){
-
-			} else {
+			//left
+			if(j != 0){
+				CurrentHex->neighbors[WEST] = &grid[i][j - 1];
+				if (i != row - 1) CurrentHex->neighbors[SOUTH_WEST] = &grid[i + 1][j - 1];
+ 				if( i != 0) CurrentHex->neighbors[NORTH_WEST] = &grid [i - 1][j - 1];
+ 
+			
+			} 
+			
+			//right
+			if(j != col - 1){
+				CurrentHex->neighbors[EAST] = &grid [i][j + 1];
+				if(i != row - 1)CurrentHex->neighbors[SOUTH_EAST] = &grid[i + 1][j + 1];
+				if (i != 0) CurrentHex->neighbors[NORTH_EAST] = &grid[i - 1][j + 1];
 
 			}
 
 		}
 	}
 
-	void;
 }
 
