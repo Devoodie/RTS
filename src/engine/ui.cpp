09afe9de8c6a45c4889c4b6a9f2c85402459bc63
Devante Coley <devantecoley802@gmail.com>
2026-07-06 15:17:04 -0400

    TODO >> add collision detection for scrol menu!

--- src/engine/ui.cpp
+++ src/engine/ui.cpp
@@ -1,118 +1,133 @@
 #include <engine/ui.hpp> 
 #include <raylib.h>
+#include <iostream>
 
 namespace ui{
 	//THIS SHOULD BE CHANGED FROM RENDER OPTIONS TO RENDER UI
-	void renderOptions(const engine::Game &engine_instance, std::unordered_map<int, Texture2D> texture_map){
-		switch(engine_instance.state){
-			case engine::FIRE:
-			case engine::OPTIONS:{
+void renderOptions(const engine::Game &engine_instance, std::unordered_map<int, Texture2D> texture_map){
+	switch(engine_instance.state){
+		case engine::FIRE:
+		case engine::OPTIONS:{
 
-				BeginMode2D(engine_instance.camera);
-				Texture2D opt_texture;
-				if(engine_instance.state == engine::FIRE){
-					opt_texture = texture_map[grid::FIRE_BUTTON];
-				} else{
-					opt_texture = texture_map[grid::MOVE_BUTTON];
-				}
+			BeginMode2D(engine_instance.camera);
+			Texture2D opt_texture;
+			if(engine_instance.state == engine::FIRE){
+				opt_texture = texture_map[grid::FIRE_BUTTON];
+			} else{
+				opt_texture = texture_map[grid::MOVE_BUTTON];
+			}
 
-				Rectangle texture_rect = {
-				     .x = 0,
-				     .y = 0,
-				     .width = (float)opt_texture.width,
-				     .height = (float)opt_texture.height,
-				};
+			Rectangle texture_rect = {
+			     .x = 0,
+			     .y = 0,
+			     .width = (float)opt_texture.width,
+			     .height = (float)opt_texture.height,
+			};
 
-			        Rectangle options = engine_instance.ui_elements[1];
-			        DrawTexturePro(opt_texture, texture_rect, options, (Vector2){.x = 0, .y = 0}, 0, RAYWHITE);
-				EndMode2D();
+			Rectangle options = engine_instance.ui_elements[1];
+			DrawTexturePro(opt_texture, texture_rect, options, (Vector2){.x = 0, .y = 0}, 0, RAYWHITE);
+			EndMode2D();
 
-				break;
-				}
-			case engine::FIRING:
+			break;
+			}
+		case engine::FIRING:
 //					  DrawText(const char *text, int posX, int posY, int fontSize, Color color);
-					  break;
-			case engine::UNIT_INFO:
-			case engine::HEX_INFO:{
-				Texture2D &info_rect = texture_map[grid::INFO_RECT];
+				  break;
+		case engine::UNIT_INFO:
+		case engine::HEX_INFO:{
+			Texture2D &info_rect = texture_map[grid::INFO_RECT];
 
-				Rectangle texture_rect = {
-				     .x = 0,
-				     .y = 0,
-				     .width = (float)info_rect.width,
-				     .height = (float)info_rect.height,
-				};
+			Rectangle texture_rect = {
+			     .x = 0,
+			     .y = 0,
+			     .width = (float)info_rect.width,
+			     .height = (float)info_rect.height,
+			};
 
-			        Rectangle options = engine_instance.ui_elements[1];
-			        DrawTexturePro(info_rect, texture_rect, options, (Vector2){.x = 0, .y = 0}, 0, RAYWHITE);
-				break;
-				}
-			case engine::SCROLL:
-			        {
-				BeginMode2D(engine_instance.camera);
-				Texture2D &elem = texture_map[grid::MOVE_BUTTON];
+			Rectangle options = engine_instance.ui_elements[1];
+			DrawTexturePro(info_rect, texture_rect, options, (Vector2){.x = 0, .y = 0}, 0, RAYWHITE);
+			break;
+			}
+		case engine::SCROLL:
+			{
+			BeginMode2D(engine_instance.camera);
+			Texture2D &elem_texture = texture_map[grid::MOVE_BUTTON];
+			const ScrollMenu &scrl_menu = engine_instance.scrl_menu;
 
-				Rectangle texture_rect = {
-				     .x = 0,
-				     .y = 0,
-				     .width = (float)elem.width,
-				     .height = (float)elem.height,
-				};
+			Rectangle dest_rect = {
+			     .x = scrl_menu.dimensions.x,
+			     .y = scrl_menu.dimensions.y,
+			     .width = (float)elem_texture.width,
+			     .height = (float)elem_texture.height,
+			};
 
-				const ScrollMenu &scrl_menu = engine_instance.scrl_menu;
+			Rectangle source_rect = {
+				.x = 0,
+				.y = 0,
+				.width = (float)elem_texture.width,
+				.height = (float)elem_texture.height,
+			};
 
-				float target_y = scrl_menu.y_pos + scrl_menu.dimensions.height;
-				if(target_y > scrl_menu.internal_height) target_y = scrl_menu.internal_height;
+			float target_y = scrl_menu.y_pos + scrl_menu.dimensions.height;
+			if(target_y > scrl_menu.internal_height) target_y = scrl_menu.internal_height;
 
-				float internal_y = scrl_menu.y_pos;
-				int elem_index = 0;
+			float current_y = scrl_menu.y_pos;
+			int elem_index = 0;
 
-				const std::vector<Rectangle> &elements = scrl_menu.elements;
+			const std::vector<Rectangle> &elements = scrl_menu.elements;
 
-				while(internal_y < target_y){
-					const Rectangle &element = elements[elem_index];
+			while(current_y < target_y){
+				const Rectangle &element = elements[elem_index];
 
-					if(internal_y > element.y + element.height){
-						elem_index += 1;
-						continue;
-					}
-
-					//ratio of how much texture to draw according to how much of the rectangle is showing
-					float draw_amount = (element.y + element.height) - internal_y;
-					draw_amount /= grid::radius / 2;
-
-					if(internal_y + draw_amount > target_y)
-
-					// WORKHERE
-					if(true) void;
+				if(current_y > element.y + element.height){
+					elem_index += 1;
+					continue;
 				}
 
-				DrawRectangle(
-						engine_instance.scrl_menu.dimensions.x, 
-						engine_instance.scrl_menu.dimensions.y, 
-						engine_instance.scrl_menu.dimensions.width, 
-						engine_instance.scrl_menu.dimensions.height, 
-						BLACK
+				//ratio of how much texture to draw according to how much of the rectangle is showing
+				float draw_amount = (element.y + element.height) - current_y;
+
+				if(current_y + draw_amount > target_y){
+					draw_amount = target_y - current_y;
+				}
+
+				dest_rect.y = current_y + scrl_menu.dimensions.y;
+				dest_rect.height = draw_amount;
+
+				source_rect.y = elem_texture.height = draw_amount;
+				source_rect.height = draw_amount;
+
+				DrawTexturePro(
+						elem_texture,
+						source_rect, 
+						dest_rect, 
+						(Vector2){.x = 0, .y = 0}, 
+						0, 
+						RAYWHITE
 						);
-				EndMode2D();
-					      //Get a bunch of shit
-			        break;
-				}
-			default:
-				break;
-		}
-	}
 
-	void renderText(const std::vector<Text> &messages){
-		for(Text message: messages){
-			DrawTextEx(
-					GetFontDefault(), 
-					message.content.c_str(), 
-					message.position, 
-					message.font_size,  //placeholder
-					5, 
-					message.text_color
-					);
-		}
+				current_y += draw_amount;
+				elem_index += 1;
+			}
+			EndMode2D();
+				      //Get a bunch of shit
+			break;
+			}
+		default:
+			break;
 	}
+}
+
+void renderText(const std::vector<Text> &messages){
+	for(Text message: messages){
+		DrawTextEx(
+				GetFontDefault(), 
+				message.content.c_str(), 
+				message.position, 
+				message.font_size,  //placeholder
+				5, 
+				message.text_color
+				);
+	}
+}
 }
